bf11aeac9445a22822d85b41ff61c473ff848ffc
Sara <sara.correagarcia@gmail.com>
2023-05-22 16:29:39 -0400

    created a file for libraries

---
+++ code/libraries.R
@@ -0,0 +1,6 @@
+# Common libraries
+# If not installed yet, run the pacman installation line
+#install.packages("pacman")
+pacman::p_load(pacman, agricolae, colorspace, cowplot, dunn.test, EBSeq, glue,
+               ggdist, ggforce, ggpubr, ggtext, here, labdsv, MASS, psych, tidyverse, 
+               RColorBrewer,roperators, tictoc, vegan, vroom)
